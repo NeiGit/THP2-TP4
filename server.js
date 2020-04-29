51506f126dea454d105eaf576659861b1e0c7533
Nicolás Ortolá <nicolas.ortola@bpmx.com.ar>
2020-04-29 12:19:01 -0300

    server change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 
 const app = express()
 dotenv.config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.use(express.json())
 
 const uri = process.env.ATLAS_URI
